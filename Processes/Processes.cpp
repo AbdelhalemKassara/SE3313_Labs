19fdea8db671ccde90aa51157d8da56da9072dd2
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-01-30 02:21:43 -0500

    add stuff

--- Processes/Processes.cpp
+++ Processes/Processes.cpp
@@ -2,33 +2,27 @@
 #include <unistd.h>
 #include <vector>
 
-void print(std::vector<int> v);
-
-int main() {
+int main()
+{
 	std::string userData;
-	std::vector<int> processes;
-	int i = 0;
 
-	while(userData != "Done") {
+	pid_t id;
+	while (userData != "Done")
+	{
 		std::cout << "Please enter some text." << std::endl;
 
 		std::cin >> userData;
 		std::cout << "'" << userData << "' was entered." << std::endl;
-			
-		processes.push_back(i);
-		i++;
 
-		print(processes);
-	}	
-
-	return 0;
-}
-
-void print(std::vector<int> v) {
-	std::string output = "";
-	for(int d = 0; d < v.size(); d++) {
-		output += ", " + std::to_string(v.at(d));
+		id = fork();
+		if (id == 0)
+		{
+			break;
+		}
 	}
 
-	std::cout << output << std::endl;
+	while (id == 0)
+	{
+	}
+	return 0;
 }
