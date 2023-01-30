9029549f5be1bbf2ec4ea6cad0cffc1aa41beef7
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-01-30 02:38:54 -0500

    add processes

--- Processes/Processes.cpp
+++ Processes/Processes.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <unistd.h>
-#include <vector>
 
-int main()
-{
+int main() {
 	std::string userData;
 
 	pid_t id;
-	while (userData != "Done")
-	{
+	while (userData != "Done") {
 		std::cout << "Please enter some text." << std::endl;
 
 		std::cin >> userData;
 		std::cout << "'" << userData << "' was entered." << std::endl;
 
 		id = fork();
-		if (id == 0)
-		{
+		if (id == 0) {//breaks out of the loop for the child process
 			break;
 		}
 	}
-
-	while (id == 0)
-	{
+	
+	//for the child processes
+	while(id == 0) {
+		std::cout << "this is a child: "<< userData << std::endl;
+		sleep(1);//makes the child process sleep for 1 second
 	}
+	
+
 	return 0;
 }
