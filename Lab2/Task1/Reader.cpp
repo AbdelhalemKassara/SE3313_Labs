4007efaa8ce1d4cfb865cbf650fa6b29007a7fa0
Abdelhalem Kassara <abdelhalemkassara@gmail.com>
2023-02-28 02:35:54 +0000

    asdfasdf

--- Lab2/Task1/Reader.cpp
+++ Lab2/Task1/Reader.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include "SharedObject.h"
+
+using namespace std;
+
+struct MyShared{
+	int threadNum;
+	int reportID;
+	int timePassed;	//stores time passed since thread was initialized. I stopped using it. 
+	int totalTime;
+	int delayT;
+};
 
 int main(void)
 {
 	std::cout << "I am a reader" << std::endl;
+	time_t first = time(0);
+	Shared<MyShared> sharedMemory("sharedMemory");
+	
+	int count = 0;
+	while (count <60){//the count exists since there isn't a way to break out of the loop
+		time_t last = time(0);
+		int timer = last-first;
+		
+		std::cout<<"Thread Number: " <<sharedMemory->threadNum << " | Report Count: " << sharedMemory->reportID << " | Time Elapsed: " << timer << " | Delay: " << sharedMemory->delayT << " | Count: " << count << std::endl;
+		
+		count++;
+		sleep(1);
+	}
+	
 }
--- Lab2/Task1/Writer.cpp
+++ Lab2/Task1/Writer.cpp
@@ -7,76 +7,104 @@
 #include <thread>
 #include <unistd.h>
 
+
 struct MyShared{
-	int delayTime;
+	int threadNum;
+	int reportID;
 	int timePassed;
-	int threadNumber;
-	int reportId;
 	int totalTime;
+	int delayT;
 };
-
-int main(void)
-{
-	std::cout << "I am a Writer" << std::endl;
-	
-	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-
-	string createThreadResp;
-	string delayResp;
-	int threadCount = 0;
-
-	while(true){
-		std::cout << "Would you like to create a writer thread?" << std::endl;
-		std::getline(std::cin, createThreadResp);
-
-		if(createThreadResp == "n") {
-			break;
-		} else {
-			std::cout << "What is the delay time for this thread?" << std::endl;
-			std::getline(std::cin, delayResp);
-
-			threadCount++;
-
-			int delay = std::stoi(delayResp);//converts string to int
-			//create thread using user input
-			thread1 = new WriterThread(threadCount, delay); //add arguments as needed
-
-		}
-	}
-
-	//example for one thread thread1
-	thread1->flag= true;
-	delete thread1;
-}
 
 class WriterThread : public Thread{
 	public:
-		int threadNum;
-		bool flag;
+		int threadNumber;
+		bool flag = false;	//false so that the loop doesn't break. 
 		int delay;
 		int reportID = 0;
-
-		WriterThread(int in, int delayT):Thread(delayT*1000){
-			//this alters the property of memory referenced object with -> notation
-			this->threadNum = in; //or whatever you want/need here
-			this->delay = delayT;
+		//int totalTime;
+		
+		WriterThread(int in, int delayTime):Thread(delayTime*1000){
+			this->threadNumber = in; 
+			this->delay = delayTime;
+			
+			//alters property of a memory referenced object with -> notation
 			
 		}
 
 		virtual long ThreadMain(void) override{
-
+			
 			//declare shared memory var so this thread can access it
 			Shared<MyShared> sharedMemory ("sharedMemory");
+			time_t firstTime = time(0);
 			
 			while(true)
 			{
-				//write to shared memory
-				this-> reportID++;//increments the report counter for every cycle
-				sleep(delay);
+				this->reportID++; //increment report counter every cycle.
+				
+				sleep(delay); //sleep the thread for the specified delay
+				time_t lastTime = time(0);
+
+				int timeElapsed = lastTime - firstTime;
+				sharedMemory-> threadNum = threadNumber; //set the shared memory location to the thread ID we are currently on
+				sharedMemory-> timePassed = timeElapsed;
+				sharedMemory-> reportID = reportID;		//update all the stuff for our shared memory location. 
+				sharedMemory->totalTime = timeElapsed*reportID;
+				sharedMemory->delayT = delay;
 				if(flag){//Exit loop to end the thread
 					break;
 				}
 			}
-		}
+		return 1;
+	}
 };
+
+
+
+int main(void)
+{
+	std::cout << "I am a Writer\n";
+	
+	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
+	
+	int threadNumber = 0;
+	std::string delayTime;
+	std::string userInput;
+	
+	WriterThread * thread1; //we use this reference to create new threads in the while statement. 
+	std::stack<WriterThread*> threadStack; 	//an empty stack. We will store the threads in here. 
+
+
+	while(true){
+		std::cout << "Do you want to create a new thread? (y/n)\n";
+		//create thread using user input
+		std::getline(std::cin, userInput);
+		
+		if (userInput == "n"){
+			break;	
+		}
+		else{
+			std::cout <<"What delay do u want on this thread? \n";
+			threadNumber++;
+			std::getline(std::cin, delayTime);
+			int delayInt = std::stoi(delayTime);
+			thread1 = new WriterThread(threadNumber, delayInt); //create a new writer thread with the thread number and the delay specified
+			threadStack.push(thread1);
+			
+		}
+	}
+	//if we are no longer in the loop then we are done collecting and creating threads. Time to delete them. 
+	for(auto i = 0; i < threadStack.size(); i++){
+		//now we need to stop the thread. So we have to set the flag to true.
+		thread1 = threadStack.top();
+		thread1->flag = true;
+		delete thread1;	//delete the thread! 
+		threadStack.pop();	//pop the top thread. 
+	}
+}
+
+
+
+
+
 
